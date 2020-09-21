6b8bcbfd5b4aa758be0a25588bd9574277b4f489
GVeldhuisen <gveldhui9675@student.landstede.nl>
2020-09-21 12:20:21 +0200

    sass toegevoegd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,21 +5,22 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
     <title>elementen pagina</title>
 </head>
-<body class="root">
+<body id="root">
 <div id="wrap_all">
     <header id="header" class="menu">
         <div class="container">
             <div class="menu-container">
                 <div class="menu-container">
                     <div class="logo-container">
-                        <p>logo</p>
+                        <img src="https://picsum.photos/200/100">
                     </div>
                     <div class="main-menu-container">
-                        <a href="#">linkie</a>
-                        <a href="#">linkie</a>
-                        <a class="button" href="#">sign in</a>
+                        <a class="menu-item" href="#">linkie</a>
+                        <a class="menu-item" href="#">linkie</a>
+                        <a class="menu-item button" href="#">sign in</a>
                     </div>
                     <div class="responsive-menu">
 

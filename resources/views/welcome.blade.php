7ef255604a67dc8d5b43727409ba5f1d03e69769
arnovdbijl <arnovanderbijl@gmail.com>
2020-09-23 19:45:19 +0200

    Login / Register to menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,28 @@
                     <div class="main-menu-container">
                         <a class="menu-item" href="#">linkie</a>
                         <a class="menu-item" href="#">linkie</a>
-                        <a class="menu-item button background-gray" href="#">sign in</a>
+                        @guest
+                                <a class="menu-item button background-gray" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            @if (Route::has('register'))
+                                <a class="menu-item button background-gray" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            @endif
+                        @else
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                        @endguest
                     </div>
                     <div class="responsive-menu">
 

49389ad26c32a4bc24c4dad45b7c59b64be28486
arnovdbijl <arnovanderbijl@gmail.com>
2020-09-28 11:36:40 +0200

    Migration users fix

--- database/migrations/2020_09_25_124939_create_users_table;.php
+++ database/migrations/2020_09_25_124939_create_users_table;.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('user', function (Blueprint $table) {
-            $table->integer('user_id');
-            $table->integer('user_admin');
-            $table->char('user_username');
-            $table->char('user_firstname');
-            $table->char('user_surname');
-            $table->string('user_email');
-            $table->char('user_password');
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
@@ -31,6 +31,6 @@ class CreateUserTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user');
+        Schema::dropIfExists('users');
     }
 }

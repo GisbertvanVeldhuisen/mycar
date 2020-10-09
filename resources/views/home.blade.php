68d8d0e39e7366f0df8bd31f45b0ba7d14067652
GisbertvanVeldhuisen <gisbertvveldhuisen@gmail.com>
2020-10-09 13:25:00 +0200

    login gefixt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,74 @@
-@extends('layouts.app')
+@include('includes/head')
+<body id="root">
+<div id="wrap_all">
+    @include('includes/header')
+    <div class="section login-succes">
+        <div class="container">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">`x</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+            <div class="heading">
+                <h3>{{ __('You are logged in!') }}</h3>
+            </div>
+            <div class="button-wrap">
+                <a href="/" class="button background-tertairy">
+                    <span class="button-title">Return to home</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="section car-overview" style="">
+        <div class="container">
+            <div class="heading">
+                <h3>car overview</h3>
+            </div>
+            <div class="grid-container">
+                <div class="column one-third">
+                    <div class="image-container">
+                        <img src="https://picsum.photos/400/400" alt="">
+                        <div class="owner">eigenaar</div>
+                        <div class="brand">brand</div>
+                        <div class="button-wrap">
+                            <a class="button background-tertairy" href="#">
+                                <span class="button-title">like</span>
+                            </a>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
+                </div>
+                <div class="column one-third">
+                    <div class="image-container">
+                        <img src="https://picsum.photos/400/400" alt="">
+                        <div class="owner">eigenaar</div>
+                        <div class="brand">brand</div>
+                        <div class="button-wrap">
+                            <a class="button background-tertairy" href="#">
+                                <span class="button-title">like</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="column one-third">
+                    <div class="image-container">
+                        <img src="https://picsum.photos/400/400" alt="">
+                        <div class="owner">eigenaar</div>
+                        <div class="brand">brand</div>
+                        <div class="button-wrap">
+                            <a class="button background-tertairy" href="#">
+                                <span class="button-title">like</span>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endsection
+</div>
+@include('includes/footer')
+@include('includes/socket')
+</body>
+</html>
+

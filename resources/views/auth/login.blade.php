8d19f532fd7b19a54ab9b0174f41347aa0e3bea7
GisbertvanVeldhuisen <gisbertvveldhuisen@gmail.com>
2020-10-28 14:38:10 +0100

    login en register fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,51 +2,54 @@
 <body id="root">
 <div id="wrap_all">
     @include('includes/header')
-    <div class="section form">
-        <div class="container">
+    <div class="main">
+        <div class="section form">
+            <div class="container">
 
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
-                <div class="heading">
-                    <h3>{{ __('Login') }}</h3>
-                </div>
-                <span for="email" class="title">{{ __('E-Mail Address') }}</span>
-                <span for="password" class="title">{{ __('Password') }}</span>
-                <input id="email" type="email" class="one-half field form-control @error('email') is-invalid @enderror"
-                       name="email"
-                       value="{{ old('email') }}" required autocomplete="email" autofocus>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="heading">
+                        <h3>{{ __('Login') }}</h3>
+                    </div>
+                    <span for="email" class="title">{{ __('E-Mail Address') }}</span>
+                    <span for="password" class="title">{{ __('Password') }}</span>
+                    <input id="email" type="email"
+                           class="one-half field form-control @error('email') is-invalid @enderror"
+                           name="email"
+                           value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                @error('email')
-                <span class="invalid-feedback" role="alert">
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                @enderror
+                    @enderror
 
-                <input id="password" type="password"
-                       class="one-half field form-control @error('password') is-invalid @enderror"
-                       name="password" required autocomplete="current-password">
+                    <input id="password" type="password"
+                           class="one-half field form-control @error('password') is-invalid @enderror"
+                           name="password" required autocomplete="current-password">
 
-                @error('password')
-                <span class="invalid-feedback" role="alert">
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                @enderror
-                <span class="title full" for="remember">
+                    @enderror
+                    <span class="title full" for="remember">
                     {{ __('Remember Me') }}
                 </span>
-                <input class="form-check-input" type="checkbox" name="remember"
-                       id="remember" {{ old('remember') ? 'checked' : '' }}>
-                <button type="submit" class="button">
-                    {{ __('Login') }}
-                </button>
-                <div class="full">
-                    @if (Route::has('password.request'))
-                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                            {{ __('Forgot Your Password?') }}
-                        </a>
-                    @endif
-                </div>
-            </form>
+                    <input class="form-check-input" type="checkbox" name="remember"
+                           id="remember" {{ old('remember') ? 'checked' : '' }}>
+                    <button type="submit" class="button">
+                        {{ __('Login') }}
+                    </button>
+                    <div class="full">
+                        @if (Route::has('password.request'))
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                        @endif
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 @include('includes/footer')

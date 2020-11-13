a7c8b37692e6156d54af8021f7cc55e75cfa42a0
jurre033 <62695020+jurre033@users.noreply.github.com>
2020-11-13 14:12:08 +0100

    Hutsie huts

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -9,19 +9,41 @@
                 </div>
             </div>
 
-            @foreach($posts as $post)
-                <p>{{$post->post_title}}</p>
-                <form action="" method="post">
-                    @csrf
-                    <input type="hidden" name="_method" value="PUT">
-                    <input type="hidden" name="post_id" value="{{ $post->post_id }}">
-                    <a href="{{ route('single-page',  $post->post_id) }}">Bekijk</a><br>
-                    <div class="button background-red">
-                        <button class="button background-red" type="submit">Delete</button>
+            <div class="section results car-overview">
+                <div class="container">
+                <div class="heading">
+                    <h3>Posts</h3>
+                </div>
+                    <div class ="grid-container">
+                        @foreach($posts as $post)
+                        <a href="{{ route('single-page',  $post->post_id) }}">
+                        <div class="column one-third">
+                            <div class="image-container">
+                            <form action="{{ route('delete-post', $post->post_id) }}" method="post">
+                                @csrf
+                                <input type="hidden" name="_method" value="DELETE">
+                                <input type="hidden" name="post_id" value="{{ $post->post_id }}">
+
+
+                                {{--<div class="button background-red">--}}
+                                    {{--<button class="button background-red" type="submit">Delete</button>--}}
+                                {{--</div>--}}
+                            </form>
+                                <img src="https://picsum.photos/400/400" alt="">
+                                <div class="owner">{{Auth::user->name}}</div>
+                                <div class="brand">{{ $post->car_brand }}</div>
+                                <div class="button-wrap">
+                                    <a class="button background-tertairy" href="#">
+                                        <span class="button-title">like</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach()
                     </div>
-                    <br>
-                </form>
-            @endforeach
+                </div>
+            </div>
+
         </div>
 
 @endsection

98d3578f05cb27dcd88484068a4ba88e18a5e3e1
GisbertvanVeldhuisen <gisbertvveldhuisen@gmail.com>
2020-11-13 14:29:10 +0100

    fixed profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,48 +2,39 @@
 
 @section ("content")
 
+    <div class="section results car-overview">
         <div class="container">
-            <div class="column full">
-                <div class="heading">
-
-                </div>
+            <div class="heading">
+                <h3>Posts</h3>
             </div>
-
-            <div class="section results car-overview">
-                <div class="container">
-                <div class="heading">
-                    <h3>Posts</h3>
-                </div>
-                    <div class ="grid-container">
-                        @foreach($posts as $post)
-                        <a href="{{ route('single-page',  $post->post_id) }}">
-                        <div class="column one-third">
-                            <div class="image-container">
+            <div class="grid-container">
+                @foreach($posts as $post)
+                    <div class="column one-third">
+                        <a class="full-link-button" href="{{ route('single-page',  $post->post_id) }}"></a>
+                        <div class="image-container">
                             <form action="{{ route('delete-post', $post->post_id) }}" method="post">
                                 @csrf
                                 <input type="hidden" name="_method" value="DELETE">
                                 <input type="hidden" name="post_id" value="{{ $post->post_id }}">
-
-
-                                {{--<div class="button background-red">--}}
-                                    {{--<button class="button background-red" type="submit">Delete</button>--}}
-                                {{--</div>--}}
-                            </form>
-                                <img src="https://picsum.photos/400/400" alt="">
-                                <div class="owner">{{Auth::user->name}}</div>
-                                <div class="brand">{{ $post->car_brand }}</div>
                                 <div class="button-wrap">
-                                    <a class="button background-tertairy" href="#">
-                                        <span class="button-title">like</span>
-                                    </a>
+                                    <button class="button background-red" type="submit">Delete</button>
                                 </div>
+                            </form>
+                            <img src="https://picsum.photos/400/400" alt="">
+                            <div class="owner"></div>
+                            <div class="brand">{{ $post->car_brand }}</div>
+                            <div class="button-wrap">
+                                <a class="button background-tertairy" href="#">
+                                    <span class="button-title">like</span>
+                                </a>
                             </div>
                         </div>
-                        @endforeach()
                     </div>
-                </div>
+                @endforeach()
             </div>
-
         </div>
+    </div>
+
+    </div>
 
 @endsection
